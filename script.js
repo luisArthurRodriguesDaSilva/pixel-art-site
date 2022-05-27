93602a60543d13ba48ff830990019fda5b803cf5
luis arthur <luisarthurlards03@gmail.com>
2022-05-27 12:44:04 -0300

    dark theme

--- script.js
+++ script.js
@@ -178,3 +178,64 @@ document.querySelector('#clear-lines').addEventListener('click',()=>{
         }
     }
 })
+
+
+const dls = document.querySelector('.material-symbols-outlined');
+if (localStorage.getItem('darkmode') == 'null'){
+    localStorage.setItem('darkmode',false);
+}
+
+const bode =document.querySelector('body');
+const titl = document.querySelector('#title');
+const btsOnPart = document.querySelectorAll('.part button');
+const darkLight = document.querySelector('#dark-light');
+
+const changeBackgrounds = (gradient) =>{
+    bode.style.backgroundImage= gradient;
+    bode.style.MozBackground = gradient;
+    bode.style.WebkitBackground = gradient;
+    bode.style.background = gradient;
+}
+
+if(localStorage.getItem('darkmode') == 'true'){
+
+    dls.innerText =  'dark_mode';
+    gradient='black';
+    dls.style.color = 'white';
+    titl.style.color = dls.style.color;
+    document.querySelector('#pixel-board').style.border = '1px solid white';
+        
+
+    changeBackgrounds(gradient);
+}
+
+const changeDarkLightMode = (e) =>{
+    console.log('passei');
+    
+
+
+    if(dls.innerText=='dark_mode') {
+        dls.innerText= 'light_mode';
+        changeBackgrounds('white');
+        dls.style.color = 'black';
+        titl.style.color = dls.style.color;
+        document.querySelector('#pixel-board').style.border = '1px solid black';
+        
+        localStorage.setItem('darkmode',false);
+    }
+    else {
+        dls.innerText =  'dark_mode';
+        changeBackgrounds('black');
+        dls.style.color = 'white';
+        titl.style.color = dls.style.color;
+
+        document.querySelector('#pixel-board').style.border = '1px solid white';
+        
+
+        localStorage.setItem('darkmode',true);
+    }
+
+    }
+
+
+darkLight.addEventListener('click',changeDarkLightMode);
